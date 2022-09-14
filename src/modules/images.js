aefd7c2ae3070b5e32809d8548865b2452501f96
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-14 17:27:19 +0200

    Fix loadImages function - now correctly waits for all images to load

--- src/modules/images.js
+++ src/modules/images.js
@@ -7,18 +7,26 @@ export const importImage = async (name) => {
 };
 
 export const loadImages = async (images) => {
-  return await Promise.all(images)
+  const imgArray = await Promise.all(images)
     .then((values) => {
+      const array = [];
       values.forEach(async (src) => {
-        const img = await new Promise((resolve) => {
+        const promise = new Promise((resolve) => {
           const image = new Image();
           image.onload = () => {
             resolve(image);
           };
           image.src = src;
         });
+        array.push(promise);
       });
-      return values;
+      return array;
     })
     .catch((err) => console.error('Error while loading images' + err));
+
+  const loadedArray = await Promise.all(imgArray).then((values) => {
+    return values.map((img) => img.src);
+  });
+
+  return loadedArray;
 };
