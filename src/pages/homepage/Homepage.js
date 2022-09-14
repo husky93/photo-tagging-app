4acc69d0510077209f77166fdd56d7da0ee9eb99
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-14 12:14:58 +0200

    Render content when images load, render spinner before they load

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,9 +1,29 @@
 import Spinner from '../../components/Spinner';
+import { useEffect, useState } from 'react';
+import { importImage, loadImages } from '../../modules/images';
 
 function Homepage() {
+  const [images, setImages] = useState(null);
+  useEffect(() => {
+    if (images === null) {
+      loadImages([
+        importImage('level-one-sm.jpg'),
+        importImage('level-two-sm.jpg'),
+        importImage('level-three-sm.jpg'),
+      ]).then((value) => setImages(value));
+    }
+  }, [images]);
   return (
     <main className="homepage">
-      <Spinner />
+      {images !== null ? (
+        <section className="content" aria-label="Content">
+          {images.map((src) => (
+            <img src={src} alt="yo" key={src} />
+          ))}
+        </section>
+      ) : (
+        <Spinner />
+      )}
     </main>
   );
 }
