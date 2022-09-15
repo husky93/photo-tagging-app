83f746427b7bedf5d871f873528ab77a0a8666d9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-15 12:02:43 +0200

    Test initial Game component functionality

---
+++ src/pages/game/__tests__/Game.test.js
@@ -0,0 +1,51 @@
+import { render, screen, act } from '@testing-library/react';
+import { Route, MemoryRouter, Routes } from 'react-router-dom';
+import imagesLoader from '../../../modules/imagesLoader';
+import Game from '../Game';
+import '@testing-library/jest-dom';
+
+jest.mock('../../../components/Header', () => () => {
+  return <header data-testid="header"></header>;
+});
+
+describe('Game', () => {
+  it('renders error when level id is wrong', () => {
+    render(
+      <MemoryRouter initialEntries={['/level-7']}>
+        <Routes>
+          <Route path="/level-:id" element={<Game />}></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+    const heading = screen.getByText(/Error 404! Page not found/i);
+    expect(heading).toBeInTheDocument();
+  });
+  it('renders spinner initially', () => {
+    render(
+      <MemoryRouter initialEntries={['/level-2']}>
+        <Routes>
+          <Route path="/level-:id" element={<Game />}></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+    const spinner = screen.getByTitle('Loading');
+    expect(spinner).toBeInTheDocument();
+  });
+  it('renders an image when it loads', async () => {
+    const imagesLoaderGetSpy = jest
+      .spyOn(imagesLoader, 'loadImages')
+      .mockResolvedValueOnce(['test.jpg']);
+    await act(async () =>
+      render(
+        <MemoryRouter initialEntries={['/level-1']}>
+          <Routes>
+            <Route path="/level-:id" element={<Game />}></Route>
+          </Routes>
+        </MemoryRouter>
+      )
+    );
+    expect(imagesLoaderGetSpy).toBeCalled();
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+  });
+});
