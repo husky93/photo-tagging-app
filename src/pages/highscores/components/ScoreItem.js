c21ea74c1da4671745fa7d0aa13f46810b7cae22
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-17 13:10:53 +0200

    Create ScoreItem component initial template

--- src/pages/highscores/components/ScoreItem.js
+++ src/pages/highscores/components/ScoreItem.js
@@ -1,5 +1,22 @@
-const ScoreItem = () => {
-  return <></>;
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+  font-size: 16px;
+`;
+
+const ScoreItem = ({ name, time }) => {
+  return (
+    <ListItem>
+      <span>{name}</span>
+      <span>{time}</span>
+    </ListItem>
+  );
+};
+
+ScoreItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
 };
 
 export default ScoreItem;
