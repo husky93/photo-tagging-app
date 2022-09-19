7c67ca6366f0fbc0b009aefcb0f4b11c34af186d
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-19 11:53:23 +0200

    Test the leaderboards section of Homepage component

--- src/pages/homepage/__tests__/Homepage.test.js
+++ src/pages/homepage/__tests__/Homepage.test.js
@@ -1,8 +1,10 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 import Homepage from '../Homepage';
 import React from 'react';
 import imagesLoader from '../../../modules/imagesLoader';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('../../../components/Header', () => () => {
   return <header data-testid="header"></header>;
@@ -12,14 +14,21 @@ jest.mock('../components/LevelPicker', () => () => {
   return <div data-testid="levelpicker"></div>;
 });
 
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return {
+    ...render(ui, { wrapper: BrowserRouter }),
+  };
+};
+
 describe('Homepage', () => {
   it('renders', async () => {
-    await act(async () => render(<Homepage />));
+    await act(async () => renderWithRouter(<Homepage />));
     const main = screen.getByRole('main');
     expect(main).toBeInTheDocument();
   });
-  it('renders spinner initially', async () => {
-    render(<Homepage />);
+  it('renders spinner initially', () => {
+    renderWithRouter(<Homepage />);
     const spinner = screen.getByTitle('Loading');
     expect(spinner).toBeInTheDocument();
   });
@@ -27,10 +36,30 @@ describe('Homepage', () => {
     const imagesLoaderGetSpy = jest
       .spyOn(imagesLoader, 'loadImages')
       .mockResolvedValueOnce(['test.jpg']);
-    await act(async () => render(<Homepage />));
-
+    await act(async () => renderWithRouter(<Homepage />));
     expect(imagesLoaderGetSpy).toBeCalled();
-    const section = await screen.findByRole('region');
-    expect(section).toBeInTheDocument();
+    await waitFor(() => {
+      const section = screen.getByRole('region');
+      expect(section).toBeInTheDocument();
+    });
+  });
+  it('has a button', async () => {
+    const imagesLoaderGetSpy = jest
+      .spyOn(imagesLoader, 'loadImages')
+      .mockResolvedValueOnce(['test.jpg']);
+    await act(async () => renderWithRouter(<Homepage />));
+    const button = screen.getByRole('button', { name: /go to leaderboards/i });
+    expect(button).toBeInTheDocument();
+  });
+  it('button navigates to highscores page on click', async () => {
+    const imagesLoaderGetSpy = jest
+      .spyOn(imagesLoader, 'loadImages')
+      .mockResolvedValueOnce(['test.jpg']);
+    renderWithRouter(<Homepage />);
+    const button = screen.getByRole('button', { name: /go to leaderboards/i });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(window.location.pathname).toBe('/highscores');
+    });
   });
 });
